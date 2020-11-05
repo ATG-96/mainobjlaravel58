aa7d9d15ebf1a8e33d2380189dc1788e29a92cdf
Buki <AbdullahTAlghanmi@gmail.com>
2020-11-05 16:51:45 +0900

    new style of graphs
    new style of graphs
    
    new style of graphs
    
    new style of graphs
    
    new style of graphs

--- app/Http/Controllers/ChartContoller.php
+++ app/Http/Controllers/ChartContoller.php
@@ -9,33 +9,39 @@ use Charts;
 class ChartContoller extends Controller
 {
     public function index(){
-        // $a=DB::table('users_line')
-        //     ->addSelect(DB::raw(`count(*)`))
-        //     ->from(`users_line`)
-        //     ->leftjoin(`safe_check`, function($join) {
-        //         $join->on(`users_line.line_userid`, `=`, `safe_check.line_id`);
-        //         })
-        //     ->where(`safe_check.line_id`, `IS`, NULL)
-        //     ->get();
-        // // print_r($a);
+
         
-        $a=DB::table('users_line')
-        ->select(DB::raw('line_userid,displayname,user_id'))
-            ->leftjoin('line_userid','users_line.line_userid', '=', 'safe_check.line_id')
-            ->where('safe_check.line_id', 'IS', NULL)
-            ->get();
-
-
-
-            
-     print_r($a);
-        $b=2;
-    $pie  =	 Charts::create('pie', 'highcharts')
-        ->title('Rep or not')
-        ->labels(['Replied', 'Not repply'])
-        ->values([$b,$a])
-        ->dimensions(510,350)
-        ->responsive(false);
-    return view('admin.chart')->with('gender', json_encode($pie))->with('pie',$pie);
-    }
+        $a = DB::select('SELECT DISTINCT
+ 
+        count(*) 
+         
+        FROM 
+         
+        public.users_line 
+         
+        LEFT JOIN 
+         
+        public.safe_check
+         
+        ON 
+         
+        public.users_line.line_userid = public.safe_check .line_id 
+         
+        WHERE 
+         
+        public.safe_check .line_id IS NULL');
+         
+        print_r($a);
+        $a = $a[0]->count;
+        print($a);
+         
+        $pie = Charts::create('pie', 'highcharts')
+         ->title('Rep or not')
+         ->labels(['Replied', 'Not repply'])
+         ->values([2,$a])
+         ->dimensions(1000,500)
+         ->responsive(false);
+         
+         return view('admin.chart')->with('pie',$pie);
+         }
 }
